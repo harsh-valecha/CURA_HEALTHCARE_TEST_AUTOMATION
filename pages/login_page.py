91e4321e64de0f82d3d5a0bf572c00cf30a80825
Harsh <harsh.valecha@yahoo.com>
2024-07-21 12:17:24 +0530

    updated login test case for positive as well as negative scenario

--- pages/login_page.py
+++ pages/login_page.py
@@ -6,6 +6,7 @@ class LoginPage:
         self.username_input = (By.ID, 'txt-username')
         self.password_input = (By.ID, 'txt-password')
         self.login_button = (By.ID, 'btn-login')
+        self.error_label = (By.XPATH,"//p[@class='lead text-danger']")
 
     def enter_username(self, username):
         self.driver.find_element(*self.username_input).send_keys(username)
@@ -15,3 +16,9 @@ class LoginPage:
 
     def click_login(self):
         self.driver.find_element(*self.login_button).click()
+
+    def get_error(self):
+        error_text = self.driver.find_element(*self.error_label).text
+        return error_text
+
+    
--- tests/test_login.py
+++ tests/test_login.py
@@ -3,7 +3,7 @@ from utils.config import Config
 from pages.login_page import LoginPage
 from users.credentials import UserCredentials
 
-
+@pytest.mark.login
 def test_login(driver):
     driver.get(Config.LOGIN_URL)
     
@@ -11,5 +11,13 @@ def test_login(driver):
     login_page.enter_username(UserCredentials.username)
     login_page.enter_password(UserCredentials.password)
     login_page.click_login()
-    expected_url = 'https://katalon-demo-cura.herokuapp.com/#appointment'
-    assert driver.current_url == expected_url , f"URL mismatch: expected {expected_url}, got {driver.current_url}"
+    try:
+        if login_page.get_error():
+            print(login_page.get_error())
+            assert False,"This is error due to invalid credentials"
+    except Exception as e:
+        print(f'Error label not found :{e.with_traceback}')
+    
+    finally:
+        expected_url = 'https://katalon-demo-cura.herokuapp.com/#appointment'
+        assert driver.current_url == expected_url , f"URL mismatch: expected {expected_url}, got {driver.current_url}"
