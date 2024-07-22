3cddfb422f3b11fbfc94d3d53805ebc69c38c61e
Harsh <harsh.valecha@yahoo.com>
2024-07-22 10:28:44 +0530

    added navigation testcases

--- pages/home_page.py
+++ pages/home_page.py
@@ -4,9 +4,34 @@ class HomePage:
     def __init__(self, driver):
         self.driver = driver
         self.makeappointment = (By.ID,'btn-make-appointment')
+        self.navigationbtn = (By.ID,"menu-toggle")
+        self.homebtn = (By.XPATH,"//a[text()='Home']")
+        self.loginbtn = (By.XPATH,"//a[text()='Login']")
 
     def click_appointment(self):
         self.driver.find_element(*self.makeappointment).click()
 
+    def click_home(self):
+        self.driver.find_element(*self.homebtn).click()
+
+    def click_login(self):
+        self.driver.find_element(*self.loginbtn).click()
+
+    def click_navigation(self):
+        self.driver.find_element(*self.navigationbtn).click()
+
+    def home_is_active(self):
+        if self.driver.find_element(*self.homebtn).is_displayed()!=0:
+            return True
+        else:
+            return False
+        
+    def login_is_active(self):
+        if self.driver.find_element(*self.loginbtn).is_displayed()!=0:
+            return True
+        else:
+            return False
+        
+    
 
     
--- tests/test_home.py
+++ tests/test_home.py
@@ -2,7 +2,7 @@ import pytest
 from utils.config import Config
 from pages.home_page import HomePage
 
-
+# simple testcase for checking home
 def test_home(driver):
     driver.get(Config.BASE_URL)
 
@@ -11,3 +11,31 @@ def test_home(driver):
     expected_url = 'https://katalon-demo-cura.herokuapp.com/profile.php#login'
     assert driver.current_url == expected_url  ,f"URL mismatch: expected {expected_url}, got {driver.current_url}"
     
+
+# testcase for vaigate to home
+@pytest.mark.navhome
+def test_home_navigation(driver):
+    driver.get(Config.BASE_URL)
+    home_page = HomePage(driver)
+    if home_page.home_is_active():
+        home_page.click_home()
+        assert driver.current_url == Config.BASE_URL
+    else:
+        home_page.click_navigation()
+        home_page.click_home()
+        assert driver.current_url == Config.BASE_URL
+
+# test case for moving from home to login
+@pytest.mark.navlogin
+def test_login_navigation(driver):
+    driver.get(Config.BASE_URL)
+    home_page = HomePage(driver)
+    if home_page.login_is_active():
+        home_page.click_login()
+        assert driver.current_url == Config.LOGIN_URL
+    else:
+        home_page.click_navigation()
+        home_page.click_login()
+        assert driver.current_url == Config.BASE_URL
+
+
